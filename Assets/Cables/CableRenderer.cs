fa83681ae6b2b50d2eedf9f4ef4a25b629ce2cf6
Jacob <logicmaster5823@gmail.com>
2024-11-26 03:49:02 +0000

    Highlights
    + New: Added AND and NAND gate highlighting when it is selected.
    + New: Added functionality for components to have highlights

--- Assets/Cables/CableRenderer.cs
+++ Assets/Cables/CableRenderer.cs
@@ -8,11 +8,14 @@ namespace Logic.Cables
         [SerializeField] private GameObject _nodeA;
         [SerializeField] private GameObject _nodeB;
         [SerializeField] private GameObject[] _cables;
+        [SerializeField] private GameObject[] _cableHighlights;
+        [SerializeField] private CableFlow _flow;
 
         private SpriteRenderer[] _cableImages;
 
         public const float CABLE_GAP = 0.5f;
         public const float CABLE_WIDTH = 0.1f;
+        public const float ADDITIONAL_HIGHLIGHT_SIZE = 0.1f;
         public readonly Color ACTIVE_COLOR = new Color(1f, 1f, 1f);
         public readonly Color INACTIVE_COLOR = new Color(0f, 0f, 0f);
 
@@ -34,6 +37,13 @@ namespace Logic.Cables
                 Render3Cable(positionA, positionB);
             else
                 Render5Cable(positionA, positionB);
+
+            if (!_flow.Selected)
+                UnrenderHighlight();
+            else if (positionA.x + CABLE_GAP * 2 < positionB.x)
+                Render3Highlight(positionA, positionB);
+            else
+                Render5Highlight(positionA, positionB);
         }
 
         /// <summary>
@@ -85,6 +95,76 @@ namespace Logic.Cables
             _cables[4].SetActive(true);
             _cables[4].transform.position = new Vector3(positionB.x - CABLE_GAP / 2, positionB.y);
             _cables[4].transform.localScale = new Vector3(CABLE_GAP, CABLE_WIDTH, 1f);
+        }
+
+        /// <summary>
+        /// Sets the highlight cables to inactive
+        /// </summary>
+        private void UnrenderHighlight()
+        {
+            _cableHighlights[0].SetActive(false);
+            _cableHighlights[1].SetActive(false);
+            _cableHighlights[2].SetActive(false);
+            _cableHighlights[3].SetActive(false);
+            _cableHighlights[4].SetActive(false);
+        }
+
+
+        /// <summary>
+        /// Renders the highlights with 3 cables
+        /// </summary>
+        /// <param name="positionA"></param>
+        /// <param name="positionB"></param>
+        private void Render3Highlight(Vector2 positionA, Vector2 positionB)
+        {
+            float xLength = positionB.x - positionA.x;
+            float yLength = positionB.y - positionA.y;
+
+            _cableHighlights[0].SetActive(true);
+            _cableHighlights[0].transform.position = new Vector3(positionA.x + xLength / 4, positionA.y);
+            _cableHighlights[0].transform.localScale = new Vector3(xLength / 2 + ADDITIONAL_HIGHLIGHT_SIZE, CABLE_WIDTH + ADDITIONAL_HIGHLIGHT_SIZE, 1f);
+
+            _cableHighlights[1].SetActive(true);
+            _cableHighlights[1].transform.position = new Vector3(positionA.x + xLength / 2, positionA.y + yLength / 2);
+            _cableHighlights[1].transform.localScale = new Vector3(CABLE_WIDTH + ADDITIONAL_HIGHLIGHT_SIZE, Mathf.Abs(yLength) + CABLE_WIDTH + ADDITIONAL_HIGHLIGHT_SIZE, 1f);
+
+            _cableHighlights[2].SetActive(true);
+            _cableHighlights[2].transform.position = new Vector3(positionB.x - xLength / 4, positionB.y);
+            _cableHighlights[2].transform.localScale = new Vector3(xLength / 2 + ADDITIONAL_HIGHLIGHT_SIZE, CABLE_WIDTH + ADDITIONAL_HIGHLIGHT_SIZE, 1f);
+
+            _cableHighlights[3].SetActive(false);
+            _cableHighlights[4].SetActive(false);
+        }
+
+        /// <summary>
+        /// Renders the highlights with 5 cables
+        /// </summary>
+        /// <param name="positionA"></param>
+        /// <param name="positionB"></param>
+        private void Render5Highlight(Vector2 positionA, Vector2 positionB)
+        {
+            float xLength = CABLE_GAP * 2 + positionA.x - positionB.x;
+            float yLength = positionB.y - positionA.y;
+
+            _cableHighlights[0].SetActive(true);
+            _cableHighlights[0].transform.position = new Vector3(positionA.x + CABLE_GAP / 2, positionA.y);
+            _cableHighlights[0].transform.localScale = new Vector3(CABLE_GAP + ADDITIONAL_HIGHLIGHT_SIZE, CABLE_WIDTH + ADDITIONAL_HIGHLIGHT_SIZE, 1f);
+
+            _cableHighlights[1].SetActive(true);
+            _cableHighlights[1].transform.position = new Vector3(positionA.x + CABLE_GAP, positionA.y + yLength / 4);
+            _cableHighlights[1].transform.localScale = new Vector3(CABLE_WIDTH + ADDITIONAL_HIGHLIGHT_SIZE, Mathf.Abs(yLength) / 2 + CABLE_WIDTH + ADDITIONAL_HIGHLIGHT_SIZE, 1f);
+
+            _cableHighlights[2].SetActive(true);
+            _cableHighlights[2].transform.position = new Vector3((positionA.x + positionB.x) / 2, (positionA.y + positionB.y) / 2);
+            _cableHighlights[2].transform.localScale = new Vector3(xLength + ADDITIONAL_HIGHLIGHT_SIZE, CABLE_WIDTH + ADDITIONAL_HIGHLIGHT_SIZE, 1f);
+
+            _cableHighlights[3].SetActive(true);
+            _cableHighlights[3].transform.position = new Vector3(positionB.x - CABLE_GAP, positionB.y - yLength / 4);
+            _cableHighlights[3].transform.localScale = new Vector3(CABLE_WIDTH + ADDITIONAL_HIGHLIGHT_SIZE, Mathf.Abs(yLength) / 2 + CABLE_WIDTH + ADDITIONAL_HIGHLIGHT_SIZE, 1f);
+
+            _cableHighlights[4].SetActive(true);
+            _cableHighlights[4].transform.position = new Vector3(positionB.x - CABLE_GAP / 2, positionB.y);
+            _cableHighlights[4].transform.localScale = new Vector3(CABLE_GAP + ADDITIONAL_HIGHLIGHT_SIZE, CABLE_WIDTH + ADDITIONAL_HIGHLIGHT_SIZE, 1f);
         }
 
         /// <summary>
