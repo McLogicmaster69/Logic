29bd0fd88adca1c0d9a0542116996e013622a037
Jacob <logicmaster5823@gmail.com>
2024-11-27 01:13:45 +0000

    Highlights
    + New: Added rest of the already existing gate highlights
    + New: Switch can select which highlight it would require

--- Assets/Nodes/Switches/Scripts/Switch.cs
+++ Assets/Nodes/Switches/Scripts/Switch.cs
@@ -6,6 +6,9 @@ public class Switch : LogicComponent
 {
     [SerializeField] private Sprite _onSprite;
     [SerializeField] private Sprite _offSprite;
+    [SerializeField] private Sprite _onHighlight;
+    [SerializeField] private Sprite _offHighlight;
+    [SerializeField] private SpriteRenderer _highlightRenderer;
 
     private SpriteRenderer _renderer;
 
@@ -18,6 +21,7 @@ public class Switch : LogicComponent
     {
         Output[0] = !Output[0];
         _renderer.sprite = Output[0] ? _onSprite : _offSprite;
+        _highlightRenderer.sprite = Output[0] ? _onHighlight : _offHighlight;
         base.Clicked();
     }
 }
