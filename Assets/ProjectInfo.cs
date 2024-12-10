0d46eeed701818456f54334c4016ef332327343c
Jacob <logicmaster5823@gmail.com>
2024-12-10 16:42:00 +0000

    Version Update

--- Assets/ProjectInfo.cs
+++ Assets/ProjectInfo.cs
@@ -2,6 +2,6 @@ namespace Logic
 {
     public static class ProjectInfo
     {
-        public const string CURRENT_VERSION = "v0.2.0";
+        public const string CURRENT_VERSION = "v0.2.1";
     }
 }
